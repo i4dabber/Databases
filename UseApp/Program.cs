1e56fc1b3035428f6d35e6d9d94fc82d0ec8741a
i4dabber <43787841+i4dabber@users.noreply.github.com>
2019-02-01 12:18:56 +0000

    Add files via upload

---
+++ UseApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationLogic;
+
+namespace UseApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PersonApp runningapp = new PersonApp();
+            runningapp.TheApp();
+
+        }
+    }
+}
